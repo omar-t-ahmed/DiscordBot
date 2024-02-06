dcbafb30dbd118b07f6715ff6723bb19c1ffa315
omar-t-ahmed <omartahmed@gmail.com>
2024-02-06 17:34:57 -0500

    register slash commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -9,14 +9,18 @@ const commands = [
     }
 ]
 
-const res = new REST({version: '10'}).setToken(process.env.TOKEN)
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        await REST.put(
+        console.log('registering slash commands')
+
+        await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             {body: commands}
             )
+
+        console.log('registered!')
     } catch (error){
         console.log(`${error}`)
     }
