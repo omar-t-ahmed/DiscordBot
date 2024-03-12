1c6e0ae0e55804881a1529605758908942a122bd
omar-t-ahmed <omartahmed@gmail.com>
2024-03-12 15:44:30 -0400

    add embed func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {Client, IntentsBitField} = require('discord.js');
+const {Client, IntentsBitField, EmbedBuilder} = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -36,6 +36,14 @@ client.on('interactionCreate', (interaction) => {
         const num2 = interaction.options.get('second-number')?.value
         
         interaction.reply(`The sum is ${num1 + num2}`)
+    }
+
+    if (interaction.commandName === 'embed') {
+        const embed = new EmbedBuilder()
+            .setTitle('embed title')
+            .setDescription('embed description')
+
+        interaction.reply({embeds: [embed]})
     }
 })
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -20,6 +20,10 @@ const commands = [
                 required: true
             }
         ]
+    },
+    {
+        name: 'embed',
+        description: 'Sends an embed'
     }
 ]
 
